52884ecc07c01ea8be1ac172ed47d9d9fffa5e76
Joshua Anickat <joanicka@microsoft.com>
2023-09-14 15:16:39 -0700

    formatting

--- index.js
+++ index.js
@@ -7,61 +7,61 @@ const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 function get_gh_labels() {
 
 }
-async function run(){
+async function run() {
   try {
-      // let token = core.getInput('token');
-      const octokit = github.getOctokit('');
+    // let token = core.getInput('token');
+    const octokit = github.getOctokit('');
 
-      const issues = (await octokit.rest.issues.listForRepo({
-          owner: 'microsoft',
-          repo: 'vscode-cmake-tools',
-          state: 'open',
-      })).data.filter(i => i.pull_request === undefined && i.labels.length === 0);
+    const issues = (await octokit.rest.issues.listForRepo({
+      owner: 'microsoft',
+      repo: 'vscode-cmake-tools',
+      state: 'open',
+    })).data.filter(i => i.pull_request === undefined && i.labels.length === 0);
 
-      // TODO get all labels
-      const labels = (await octokit.rest.issues.listLabelsForRepo({
-        owner: 'microsoft',
-        repo: 'vscode-cmake-tools',
-      })).data.map(i => i.name).join("', '");
+    // TODO get all labels
+    const labels = (await octokit.rest.issues.listLabelsForRepo({
+      owner: 'microsoft',
+      repo: 'vscode-cmake-tools',
+    })).data.map(i => i.name).join("', '");
 
-      console.log(labels);
+    console.log(labels);
 
-      let apiKey = "";//core.getInput('api_key');
-      const client = new OpenAIClient(
-        'https://americasopenai.azure-api.net',
-        new AzureKeyCredential(apiKey)
+    let apiKey = "";//core.getInput('api_key');
+    const client = new OpenAIClient(
+      'https://americasopenai.azure-api.net',
+      new AzureKeyCredential(apiKey)
+    );
+
+    var chatcmpl = {}
+    var issueLabel = {}
+
+    // iterate through issues to label
+    issues.forEach(async (issue) => {
+      // todo maybe play with how title and body are represented
+      const issueText = issue.title + '. ' + issue.body.replace(/\n/g, ' ');
+      const messages = [{ 'role': 'system', 'content': `You are a bot that matches one or more labels in a single line to each provided text in a single line: ${labels}` },];
+      messages.push({ "role": "user", "content": issueText });
+      const completion = client.listChatCompletions(
+        "gpt-35-turbo-16k",
+        messages
       );
 
-      var chatcmpl = {}
-      var issueLabel = {}
-
-      // iterate through issues to label
-       issues.forEach(async (issue) => {
-        // todo maybe play with how title and body are represented
-        const issueText = issue.title + '. ' + issue.body.replace(/\n/g, ' ');
-        const messages = [{'role': 'system', 'content': `You are a bot that matches one or more labels in a single line to each provided text in a single line: ${labels}`},];
-        messages.push({"role": "user", "content": issueText});
-        const completion = client.listChatCompletions(
-          "gpt-35-turbo-16k",
-          messages
-        );
-
-        for await (const event of completion) {
-          for (const choice of event.choices) {
-            const delta = choice.delta?.content;
-            if (delta !== undefined) {
-              const resp = delta.replace(/\n/g, ' ');
-              if (chatcmpl[event.id]) {
-                chatcmpl[event.id] += resp;
-              } else {
-                chatcmpl[event.id] = resp;
-              }
+      for await (const event of completion) {
+        for (const choice of event.choices) {
+          const delta = choice.delta?.content;
+          if (delta !== undefined) {
+            const resp = delta.replace(/\n/g, ' ');
+            if (chatcmpl[event.id]) {
+              chatcmpl[event.id] += resp;
+            } else {
+              chatcmpl[event.id] = resp;
             }
           }
-          issueLabel[issue.number] = `[ISSUE ${issue.number}][${issue.title}][${chatcmpl[event.id]}]`
-          console.log(`[ISSUE ${issue.number}][${issue.title}][${chatcmpl[event.id]}]`);
         }
-      });
+        issueLabel[issue.number] = `[ISSUE ${issue.number}][${issue.title}][${chatcmpl[event.id]}]`
+        console.log(`[ISSUE ${issue.number}][${issue.title}][${chatcmpl[event.id]}]`);
+      }
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
